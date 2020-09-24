676440a2a2b08205dc9b7aabc5d7d9de3920d4c8
Marlon Beltran <Marlon Beltran>
2020-09-24 14:18:04 -0600

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,69 @@
+feather.replace();
+
+const controls = document.querySelector('.controls');
+const cameraOptions = document.querySelector('.video-options>select');
+const video = document.querySelector('video');
+const canvas = document.querySelector('canvas');
+const screenshotImage = document.querySelector('img');
+const buttons = [...controls.querySelectorAll('button')];
+let streamStarted = false;
+
+const [play, pause, screenshot] = buttons;
+
+const constraints = {
+    video: {
+        width: {
+            min: 1280,
+            ideal: 1920,
+            max: 2560,
+        },
+        height: {
+            min: 720,
+            ideal: 1080,
+            max: 1440,
+        },
+    }
+};
+
+const getCameraSelection = async () => {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const videoDevices = devices.filter(device => device.kind === 'videoinput');
+    const options = videoDevices.map(videoDevice => {
+        return `<option value="${videoDevice.deviceId}">${videoDevice.label}`
+    });
+    cameraOptions.innerHTML = options.join('');
+};
+
+play.onclick = () => {
+    if (streamStarted) {
+        video.pay();
+        play.classList.add('d-none');
+        pause.classList.remove('d-none');
+        return;
+    }
+
+    if ('mediaDevices' in navigator && navigator.mediaDevices.geUserMedia) {
+        const updatedConstraints = {
+            ...contraints,
+            deviceId: {
+                exact: cameraOptions.value
+            }
+        };
+        startStream(updatedConstraints);
+    }
+};
+
+const startStream = async (constraints) => {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    handleStream(stream);
+}
+
+const handleStream = (stream) => {
+    video.srcObject = stream;
+    play.classList.add('d-none');
+    pause.classList.remove('d-none');
+    screenshot.classList.remove('d-none');
+    streamStarted = true;
+};
+
+getCameraSelection();
